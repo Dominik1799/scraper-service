bdc68aaf661bdd813621ba92ac8154da2c400b0e
JKBGIT1 <jakubhlavacka123@gmail.com>
2024-08-08 21:01:36 +0200

    fix not working import by changing run cmd

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 from settings import ROOT_PATH
 from routers import scraper_router
@@ -6,6 +7,6 @@ app = FastAPI(root_path=ROOT_PATH, title="adversea scraper service")
 
 app.include_router(scraper_router.router)
 
-@app.get("")
-async def root():
-    return "Hello from court cases service. Visit /docs"
+@app.get("/")
+def root():
+    return "Hello from scraper service. Visit /docs"
--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-ROOT_PATH = str(os.getenv("ROOT_PATH") or "/")
+ROOT_PATH = str(os.getenv("ROOT_PATH") or "")
 TOR_PROXIES = list((os.getenv("TOR_PROXIES") or "http://127.0.0.1:8888,http://127.0.0.1:8800").split(","))
 MAX_PROXY_RETRIES = int(os.getenv("MAX_PROXY_RETRIES") or 2)
 LOG_LEVEL = int(os.getenv("LOG_LEVEL") or logging.INFO)
